3e2ba2f58bf44ea7fad19259c50b5e29a7fb91af
sunjingzhao <sunjz@cbim.com.cn>
2022-09-05 15:34:49 +0800

    test code

--- src/main.cpp
+++ src/main.cpp
@@ -32,16 +32,16 @@ public:
     std::shared_ptr<std::string> spContent;
     std::unordered_set<float> arr;
     std::unordered_map<int, int> mapKV;
-    glm::dvec2 v2;
+    glm::vec2 v2;
     CBIM_JSON_HELPER(Name, test, students, master, spContent, arr, mapKV, v2)
     CBIM_JSON_HELPER_DEFAULT("test=123")
 };
 
 string sjson = R"({
-    "v2": {
-        "x": 1.0,
-        "y": 2.0
-    },
+    "v2": [
+        1.0,
+        2.0
+    ],
     "arr": [
         1.0,
         1.1,
@@ -81,22 +81,26 @@ string sjson = R"({
 
 struct Geo
 {
-    std::string name;
-    glm::mat4 m = glm::mat4(1.0f);
-    CBIM_JSON_HELPER(name, m)
-    CBIM_JSON_HELPER_DEFAULT(name="cube")
+    std::unordered_map<std::string, size_t> people;
+    CBIM_JSON_HELPER(people)
+};
+
+struct Table
+{
+    std::vector<float> vecNames;
+    CBIM_JSON_HELPER(vecNames)
 };
 
 int main()
 {
     //std::cout << sjson << std::endl;
-//    TheClass my;
-//    JsonHelper::JsonToObject(my, sjson);
+    TheClass my;
+    JsonSerializationHelper::JsonToObject(my, sjson);
 
     std::string str;
-//    JsonHelper::ObjectToJson(my, str);
-//    std::cout << str << std::endl;
-//    std::cout << str.size() << std::endl;
+    JsonSerializationHelper::ObjectToJson(my, str, 4);
+    std::cout << str << std::endl;
+    std::cout << str.size() << std::endl;
 
 //    std::vector<uint8_t> buf;
 //    JsonHelper::ObjectToJsonCbor(my, buf);
@@ -106,12 +110,23 @@ int main()
 //    JsonHelper::JsonCborToObject(mm, buf);
 //    std::cout << mm.Name << std::endl;
 
-    Geo g;
-//    g.name = "大家";
-    JsonSerializationHelper::ObjectToJson(g, str, 4);
-    std::cout << str << std::endl;
+//    Geo g;
+////    g.name = "大家";
+//    JsonSerializationHelper::ObjectToJson(g, str, 4);
+//    std::cout << str << std::endl;
 
-    Geo g0;
-    JsonSerializationHelper::JsonToObject(g0, str);
+//    Geo g0;
+//    JsonSerializationHelper::JsonToObject(g0, str);
+
+//    Geo g;
+//    g.people = {{"Ann",25},{"Bill", 46}, {"Jack", 77}, {"Jack", 32},{"Jill", 32}, {"Ann", 35} };
+//    JsonSerializationHelper::ObjectToJson(g, str, 4);
+
+
+//    Table tb;
+//    tb.vecNames = {1, 2};
+//    JsonSerializationHelper::ObjectToJson(tb, str, 4);
+//    std::cout << str << std::endl;
+
     return 0;
 }
